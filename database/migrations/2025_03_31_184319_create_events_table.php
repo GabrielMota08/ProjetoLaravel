8b5874f8a395586d28b5e9c7b777c1388dd1b129
GabrielMota08 <gabrielmota0812silva@gmail.com>
2025-04-01 13:58:13 -0300

    Remoção de arquivos clonados

--- database/migrations/2025_03_31_184319_create_events_table.php
+++ database/migrations/2025_03_31_184319_create_events_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->string("title");
+            $table->text("description");
+            $tablr->string("city");
+            $table->boolean("private");
             $table->timestamps();
         });
     }
